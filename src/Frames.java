079ffdcf6c1c678c1890198c7b47cd923ea65b77
Siarhei Bukhavets <sbukhovets@gmail.com>
2021-03-30 22:16:15 +0300

    Added Switching Between Frames test.

--- src/Frames.java
+++ src/Frames.java
@@ -1,5 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Frames {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","c:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 
+        // launch browser and open link
+        driver.get("http://demo.guru99.com/selenium/deprecated.html");
+
+        // switch to frame on page by name of it
+        driver.switchTo().frame("classFrame");
+
+        // find element on the page by linkText and click it
+        driver.findElement(By.linkText("Index")).click();
+
+        // close session
+        driver.close();
+
+        System.out.println("Test Passed!");
     }
 }
