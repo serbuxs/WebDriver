6809f645fa59860834bb509419fada90b9de932d
sbukhovets <sbukhovets@gmail.com>
2021-03-31 16:51:29 +0300

    Added retrieve the element text on pop-up window. After that window is closing.

--- src/PopUp.java
+++ src/PopUp.java
@@ -1,5 +1,32 @@
+// Get alert message from pop-up window
+// and display it in console
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PopUp {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","c:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 
+        String alertMessage = "";
+
+        // launch browser with url
+        driver.get("http://jsbin.com/usidix/1");
+
+        // find element by cssSelector
+        driver.findElement(By.cssSelector("input[value=\"Go!\"]")).click();
+
+        // get text from alert message at pop-up window
+        alertMessage = driver.switchTo().alert().getText();
+
+        // close pop-up window by click OK button
+        driver.switchTo().alert().accept();
+
+        System.out.println("Message text: " + alertMessage);
+
+        // close browser
+        driver.close();
     }
 }
